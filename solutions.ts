a88e557cd456477f9401eb35e5983a69cb04a9a1
minhazchy101 <minhazchowdhury101@gmail.com>
2026-05-07 13:01:03 +0600

    Initial commit

---
+++ solutions.ts
@@ -0,0 +1,11 @@
+// Problem 1:
+
+function filterEvenNumbers (numbers: number[]): number[]{
+    return numbers.filter(num => num % 2 === 0)
+}
+// console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]))
+
+// Problem 2:
+
+
+
