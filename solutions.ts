15d0d5a59e4d46fa4ca89d6793f6bca6dbe3921e
minhazchy101 <minhazchowdhury101@gmail.com>
2026-05-07 15:34:34 +0600

    solve: implement Person and Student classes with inheritance

--- solutions.ts
+++ solutions.ts
@@ -45,6 +45,42 @@ function toggleReadStatus(book: Book) {
 }
 
 const myBook = { title: "TypeScript Guide", author: "Jane Doe", publishedYear: 2024 };
-console.log(toggleReadStatus(myBook))
+// console.log(toggleReadStatus(myBook))
+
+
+
+//Problem 6: 
+class Person {
+    name: string
+    age:  number
+
+    constructor(
+        name: string,
+    age:  number){
+        this.name = name,
+        this.age = age
+
+    }
+}
+
+class Student extends Person {
+    grade: string
+
+    constructor(
+         name: string,
+         age:  number,
+          grade: string
+    ){
+        super(name, age)
+        this.grade = grade
+    }
+
+    getDetails (): string{
+        return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`
+    }
+}
+
+const student = new Student("Alice", 20, "A");
+// console.log(student.getDetails());
 
 
